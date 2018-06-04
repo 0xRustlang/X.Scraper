1f29f1601cb6462552d1e3be8432f06809ef2edc
Vladislav Kamenev <mazahakaforever@ya.ru>
2018-06-04 04:20:42 +0300

    Submodules reogranised

--- swagger.sh
+++ swagger.sh
@@ -1,2 +1,3 @@
-docker run --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli generate -i /local/openapi/swagger.yml -l typescript-node -o /local/src/xmeterapi/
+docker run --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli generate -i /local/xmeterapi/swagger.yml -l typescript-node -o /local/src/xmeterapi/
+docker run --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli generate -i /local/xscraperapi/swagger.yml -l nodejs-server -o /local/src/xscraperapi/
 
